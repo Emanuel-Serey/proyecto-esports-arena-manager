6a9153e907216415bac9742c2ece07640bf101fe
Emanuel Serey <em.serey@duocuc.cl>
2026-05-13 09:28:03 -0400

    Se implemento el manejo de excepciones en team-service

---
+++ team-service/src/main/java/cl/duoc/esports/teamservice/exceptions/EquipoException.java
@@ -0,0 +1,8 @@
+package cl.duoc.esports.teamservice.exceptions;
+
+public class EquipoException extends RuntimeException {
+
+    public EquipoException(String message) {
+        super(message);
+    }
+}
