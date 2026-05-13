1f1d555327b1e6540eacdaa7042d57561f394642
Emanuel Serey <em.serey@duocuc.cl>
2026-05-13 02:01:33 -0400

    Se agrega manejo de excepciones para user-service

---
+++ user-service/src/main/java/cl/duoc/esports/userservice/exceptions/UsuarioException.java
@@ -0,0 +1,8 @@
+package cl.duoc.esports.userservice.exceptions;
+
+public class UsuarioException extends RuntimeException {
+
+    public UsuarioException(String message) {
+        super(message);
+    }
+}
