fc25c3442cbe38dcace7a631d630838ce1e40bb2
Emanuel Serey <em.serey@duocuc.cl>
2026-05-14 01:40:10 -0400

    Se corrige error con Feign no activado en la clase principal

--- team-service/src/main/java/cl/duoc/esports/teamservice/TeamServiceApplication.java
+++ team-service/src/main/java/cl/duoc/esports/teamservice/TeamServiceApplication.java
@@ -2,12 +2,13 @@ package cl.duoc.esports.teamservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class TeamServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TeamServiceApplication.class, args);
 	}
-
 }
