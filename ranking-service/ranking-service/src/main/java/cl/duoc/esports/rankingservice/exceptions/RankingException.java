19c1e33112004130b787c28668a8dd236a80f51c
KyroxTxu <benja.gutierreza@duocuc.cl>
2026-05-21 22:19:23 -0400

    Se agregan manejo de excepciones para ranking-service

---
+++ ranking-service/ranking-service/src/main/java/cl/duoc/esports/rankingservice/exceptions/RankingException.java
@@ -0,0 +1,8 @@
+package cl.duoc.esports.rankingservice.exceptions;
+
+public class RankingException extends RuntimeException {
+
+    public RankingException(String message) {
+        super(message);
+    }
+}
