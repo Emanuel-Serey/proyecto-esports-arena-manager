432c197c55ce68b6731b6628b06cf46a92c46fd1
KyroxTxu <benja.gutierreza@duocuc.cl>
2026-05-21 21:50:50 -0400

    Se agregan manejo de excepciones para result-service

--- result-service/result-service/src/main/java/cl/duoc/esports/resultservice/controllers/ApiExceptionHandler.java
+++ result-service/result-service/src/main/java/cl/duoc/esports/resultservice/controllers/ApiExceptionHandler.java
@@ -1,4 +1,37 @@
 package cl.duoc.esports.resultservice.controllers;
 
+import cl.duoc.esports.resultservice.exceptions.ResultadoException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
 public class ApiExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException ex) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        for (FieldError fieldError : ex.getBindingResult().getFieldErrors()) {
+            errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(ResultadoException.class)
+    public ResponseEntity<Map<String, String>> handleResultadoException(ResultadoException ex) {
+        Map<String, String> error = new HashMap<>();
+
+        error.put("error", ex.getMessage());
+
+        return ResponseEntity.badRequest().body(error);
+    }
 }
---
+++ result-service/result-service/src/main/java/cl/duoc/esports/resultservice/exceptions/ResultadoException.java
@@ -0,0 +1,8 @@
+package cl.duoc.esports.resultservice.exceptions;
+
+public class ResultadoException extends RuntimeException {
+
+    public ResultadoException(String message) {
+        super(message);
+    }
+}
