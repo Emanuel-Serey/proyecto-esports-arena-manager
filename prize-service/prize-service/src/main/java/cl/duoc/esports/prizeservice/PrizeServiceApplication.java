2d07dbecf03746c0d1d5e5f3ebd0b1349113cea9
KyroxTxu <benja.gutierreza@duocuc.cl>
2026-05-21 22:45:23 -0400

    Se agrega Feign en clase principal

--- prize-service/prize-service/src/main/java/cl/duoc/esports/prizeservice/PrizeServiceApplication.java
+++ prize-service/prize-service/src/main/java/cl/duoc/esports/prizeservice/PrizeServiceApplication.java
@@ -2,12 +2,13 @@ package cl.duoc.esports.prizeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class PrizeServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PrizeServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(PrizeServiceApplication.class, args);
+    }
 }
