9724e7680e087ab1b6a2a3d39045e9a23c442289
KyroxTxu <benja.gutierreza@duocuc.cl>
2026-05-21 22:43:10 -0400

    Se agregan manejo de excepciones para prize-service

---
+++ prize-service/prize-service/src/main/java/cl/duoc/esports/prizeservice/exceptions/PremioException.java
@@ -0,0 +1,8 @@
+package cl.duoc.esports.prizeservice.exceptions;
+
+public class PremioException extends RuntimeException {
+
+    public PremioException(String message) {
+        super(message);
+    }
+}
