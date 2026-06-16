873ae7c2da23011cc662dd4a029df1ed724b27f0
sebai <sebaignaciokr20@gmail.com>
2026-06-16 18:24:17 -0400

    se agregan Swagger y documentacion de errores en sanction-service

--- sanction-service/src/main/java/cl/duoc/esports/sanctionservice/config/SwaggerConfig.java
+++ sanction-service/src/main/java/cl/duoc/esports/sanctionservice/config/SwaggerConfig.java
@@ -1,4 +1,19 @@
 package cl.duoc.esports.sanctionservice.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI sanctionServiceOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Sanction Service API")
+                        .version("1.0")
+                        .description("Documentación de endpoints del microservicio de sanciones del sistema eSports Arena Manager"));
+    }
 }
